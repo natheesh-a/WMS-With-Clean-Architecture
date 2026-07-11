c5b9c351771dfdd864b7d274ddd16d6d634905cd
Natheesh <natheesh.ayyadurai@avaniko.com>
2026-07-11 10:09:09 +0530

    Source initial Commit

---
+++ src/WMS.Api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ tests/WMS.Api.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace WMS.Api.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
